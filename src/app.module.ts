0987d1ddd1ac5b382ec1883822f4340b4aa1a019
Fernando Herrera <fernando.herrera85@gmail.com>
2022-10-05 15:50:22 -0400

    SSL added

--- src/app.module.ts
+++ src/app.module.ts
@@ -57,6 +57,12 @@ import { ListItemModule } from './list-item/list-item.module';
 
     TypeOrmModule.forRoot({
       type: 'postgres',
+      ssl: ( process.env.STATE === 'prod' ) 
+        ? {
+          rejectUnauthorized: false,
+          sslmode: 'require',
+        } 
+        : false as any,
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
@@ -83,4 +89,16 @@ import { ListItemModule } from './list-item/list-item.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor() {
+
+    console.log("STATE",  process.env.STATE );
+    console.log("host",  process.env.DB_HOST );
+    console.log("port",  +process.env.DB_PORT );
+    console.log("username",  process.env.DB_USERNAME );
+    console.log("password",  process.env.DB_PASSWORD );
+    console.log("database",  process.env.DB_NAME );
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,9 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen( PORT );
+  
+  console.log(`App running on port ${ PORT }`);
 }
 bootstrap();
